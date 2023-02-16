f841d32bebad6a87cafaf071374bbf80e6a594cc
HanSangWook <qazws78941@gmail.com>
2023-02-17 00:41:23 +0900

    2023년 02월 17일 블로그 포스팅 종료

--- lib/src/pages/text_field.dart
+++ lib/src/pages/text_field.dart
@@ -6,13 +6,39 @@ class TextFieldPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TextField'),
+      backgroundColor: Colors.grey[100],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _buildCustomTextField('사용자 이름, 이메일 주소 또는 휴대폰 번호'),
+              SizedBox(
+                height: 15,
+              ),
+              _buildCustomTextField('비밀번호'),
+            ]),
       ),
-      body: Center(
-          child: TextField(
-        decoration: InputDecoration(border: InputBorder.none),
-      )),
+    );
+  }
+
+  Widget _buildCustomTextField(String label) {
+    final focusColor = Colors.black;
+    final unfocusColor = Colors.grey;
+    return TextField(
+      cursorColor: focusColor,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle_rounded),
+          focusColor: focusColor,
+          fillColor: Colors.white,
+          filled: true,
+          labelStyle: TextStyle(color: focusColor),
+          focusedBorder: OutlineInputBorder(),
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: unfocusColor)),
+          border: InputBorder.none,
+          labelText: label),
     );
   }
 }
